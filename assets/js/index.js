defa71efd2a9eda43bc4580f3502072639e1401c
Vinícius Gomes de Souza <vgdsouza@live.com>
2021-12-29 10:53:15 -0300

    There will be no scrollbar
    Signed-off-by: Vinícius Gomes de Souza <vgdsouza@live.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,4 @@
 window.addEventListener("load", function() {
-    function scrollbarWidth() {
-        let htmlRoot = document.querySelector(":root");
-        let scrollbar = window.innerWidth - htmlRoot.offsetWidth;
-        htmlRoot.style.setProperty("--scrollbar", scrollbar + "px");
-    }
-
-    scrollbarWidth();
-
     let sasjs = new SASjs.default({
         appLoc: "/Public/app/ICL",
         serverType: "SASVIYA",
