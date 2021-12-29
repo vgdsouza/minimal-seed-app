5af9571b28d9b93a6e4e0420245fc4beb5fb2fd6
Vinícius Gomes de Souza <vgdsouza@live.com>
2021-12-29 11:48:44 -0300

    List showing
    Signed-off-by: Vinícius Gomes de Souza <vgdsouza@live.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,41 @@ window.addEventListener("load", function() {
         debug: false,
         loginMechanism: "Redirected"
     });
+
+    sasjs.checkSession().then((res) => {
+        if (res.isLoggedIn) {
+            document.querySelector("#notLoggedIn").style.display = "none";
+            document.querySelector("#loggedIn").style.display = "flex";
+        }
+    })
+
+    function createSelectListas(listas) {
+        const htmlSelect = document.querySelector("#tableselect");
+        listas.forEach((lista) => {
+            const option = new Option();
+            option.value = lista['table_reference'];
+            option.text = lista['list_name'];
+            htmlSelect.options.add(option);
+        });
+    }
+
+    function appinit() {
+        sasjs.request("services/common/appinit", null, undefined, loginRequired).then((response) => {
+            let responseJson;
+
+            try {
+                responseJson = response;
+            } catch (e) {
+                console.error(e);
+            }
+
+            if (responseJson && responseJson.status === 449) {
+                appinit();
+            } else if (responseJson && responseJson.areas) {
+                createSelectListas(responseJson.areas);
+            }
+        });
+    }
+
+    appinit();
 })
