dea0f61d4e9d2c8a90b76a91aa59b4a141579002
Vinícius Gomes de Souza <vgdsouza@live.com>
2021-12-29 10:50:46 -0300

    frontend done except tableview
    Signed-off-by: Vinícius Gomes de Souza <vgdsouza@live.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,17 @@
-document.querySelector("#body").addEventListener("load", function() {
-    function setScrollbarWidth() {
-        var element = document.querySelector(':root');
-        var width = window.innerWidth - element.offsetWidth;
-        element.style.setProperty('--scrollbar-width', width + 'px');
+window.addEventListener("load", function() {
+    function scrollbarWidth() {
+        let htmlRoot = document.querySelector(":root");
+        let scrollbar = window.innerWidth - htmlRoot.offsetWidth;
+        htmlRoot.style.setProperty("--scrollbar", scrollbar + "px");
     }
 
-    setScrollbarWidth();
+    scrollbarWidth();
 
     let sasjs = new SASjs.default({
-        appLoc: '/Public/app/ICL',
-        serverType: 'SASVIYA',
-        serverUrl: '',
+        appLoc: "/Public/app/ICL",
+        serverType: "SASVIYA",
+        serverUrl: "",
         debug: false,
-        loginMechanism: 'Redirected'
+        loginMechanism: "Redirected"
     });
-
-    console.log("Foo");
 })
- console.log("Blah");
