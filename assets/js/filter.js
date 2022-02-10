6a722cb1de4b864649253d821ac87469c891959b
Vinícius Gomes de Souza <vgdsouza@live.com>
2022-02-10 13:37:24 -0300

    Filter prototype
    Signed-off-by: Vinícius Gomes de Souza <vgdsouza@live.com>

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,3 +1,56 @@
 window.addEventListener("load", function () {
+    /** @type {HTMLInputElement} */
+    const tablefilterinput = document.querySelector("#tablefilterinput");
 
+    /** @type {HTMLTableSectionElement} */
+    const tbody = document.querySelector("#tbody");
+
+    tablefilterinput.addEventListener("change", filtraTabela);
+
+    function filtraTabela() {
+        /** @type {String} */
+        let textoBusca;
+        /** @type {String} */
+        let textoCelula;
+        /** @type {HTMLTableRowElement} */
+        let linhaAtual;
+        /** @type {HTMLTableCellElement} */
+        let celulaAtual;
+        /** @type {Boolean} */
+        let buscaEncontrada;
+
+        textoBusca = tratamentoTextual(tablefilterinput.value);
+
+        tbody.childNodes.forEach((linha) => {
+            linhaAtual = linha;
+            buscaEncontrada = false;
+
+            linhaAtual.childNodes.forEach((celula) => {
+                celulaAtual = celula;
+                textoCelula = tratamentoTextual(celulaAtual.innerText);
+
+                if (textoCelula.indexOf(textoBusca) > -1) {
+                    buscaEncontrada = true;
+                    break;
+                }
+            });
+
+            if (buscaEncontrada) {
+                linhaAtual.style.display = "";
+            } else {
+                linhaAtual.style.display = "none";
+            }
+        });
+    }
+
+    function tratamentoTextual(texto) {
+        // n - Normalizado
+        let texto_n = texto.normalize("NFD");
+        // sa - Sem Acentos
+        let texto_nsa = texto_n.replace(/[\u0300-\u036f]/g, "");
+        // u - Uppercase
+        let texto_nsau = texto_nsa.toUpperCase();
+
+        return texto_nsau;
+    }
 });
