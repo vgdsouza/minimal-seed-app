619417db64403546d3196b06794eba323cad32b8
Vinícius Gomes de Souza <vgdsouza@live.com>
2022-02-16 03:46:58 -0300

    Pending SQL
    Signed-off-by: Vinícius Gomes de Souza <vgdsouza@live.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,6 +23,7 @@ window.addEventListener("load", function () {
         }
     });
 
+
     /* APPINIT */
     async function appinit() {
         await sasjs.request("services/common/appinit", null).then((res) => {
@@ -53,11 +54,16 @@ window.addEventListener("load", function () {
 
         listas.forEach((lista) => {
             const btnVer = document.querySelector(`#${lista['TABLE_REFERENCE']}_VER`);
-            const btnEditar = document.querySelector(`#${lista['TABLE_REFERENCE']}_EDITAR`);
-            const btnExcluir = document.querySelector(`#${lista['TABLE_REFERENCE']}_EXCLUIR`);
             const btnVerVoltar = document.querySelector("#ver_lista button");
+
+            const btnEditar = document.querySelector(`#${lista['TABLE_REFERENCE']}_EDITAR`);
             const btnEditarVoltar = document.querySelector("#editar_lista button");
+
+            const btnExcluir = document.querySelector(`#${lista['TABLE_REFERENCE']}_EXCLUIR`);
             const btnExcluirVoltar = document.querySelector("#excluir_lista button:last-child");
+
+            const btnCriar = document.querySelector("#criar_lista_btn");
+            const btnCriarVoltar = document.querySelector("#criar_lista button:last-child");
 
             btnVer.addEventListener("click", function () {
                 _spinner.style.display = "";
@@ -89,6 +95,15 @@ window.addEventListener("load", function () {
 
             btnExcluirVoltar.addEventListener("click", function () {
                 document.querySelector("#excluir_lista").style.display = "none";
+                _listas.style.display = "";
+            });
+
+            btnCriar.addEventListener("click", function () {
+                renderCreate();
+            });
+
+            btnCriarVoltar.addEventListener("click", function () {
+                document.querySelector("#criar_lista").style.display = "none";
                 _listas.style.display = "";
             });
         })
@@ -157,43 +172,133 @@ window.addEventListener("load", function () {
     /* GETDATA END */
 
 
-    /* UPDATEDATA */
+    /* UPLOADDATA */
     function renderUpdate(value) {
         const _editar_lista = document.querySelector("#editar_lista");
         const _fileInput = document.querySelector("#editar_lista .input-group input");
         const _btnEnviar = document.querySelector("#editar_lista .input-group button");
 
         _btnEnviar.addEventListener("click", function () {
-            updatedata(_fileInput, value);
+            uploaddata(_fileInput, value);
         });
 
         _spinner.style.display = "none";
         _editar_lista.style.display = "";
     }
 
-    async function updatedata(input, value) {
+    async function uploaddata(input, value) {
         const myfile = input.files[0];
 
         if (myfile) {
-            await sasjs.uploadFile('services/common/uploaddata', [{ "file": newFile, "fileName": myfile.name }], { "tableRef": value });
+            await sasjs.uploadFile('services/common/uploaddata', [{ "file": myfile, "fileName": myfile.name }], { "tableRef": value }).then((res) => {
+                console.log(res);
+            });
         }
     }
-    /* UPDATEDATA END */
+    /* UPLOADDATA END */
 
 
-    /* DISABLELIST */
+    /* DISABLEDATA */
     function renderDelete(lista, value) {
         const _excluir_lista = document.querySelector("#excluir_lista");
         const _span = document.querySelector("#excluir_lista div span");
         const _btnConfirmar = document.querySelector("#excluir_lista button:first-child");
 
         _btnConfirmar.addEventListener("click", function () {
-            sasjs.request("services/common/disablelist", { [value]: [{}] });
+            disabledata(value);
         });
 
         _span.innerText = lista;
 
         _excluir_lista.style.display = "";
     }
-    /* DISABLELIST END */
+
+    async function disabledata(value) {
+        await sasjs.request("services/common/disabledata", { [value]: [{}] }).then((res) => {
+            console.log(res);
+        });
+    }
+    /* DISABLEDATA END */
+
+
+    /* CREATEDATA */
+    function renderCreate() {
+        const _criar_lista = document.querySelector("#criar_lista");
+        const _btnConfirmar = document.querySelector("#criar_lista button:first-child");
+
+        const _criar_lista_name = document.querySelector("#criar_lista_name");
+        const _criar_lista_file = document.querySelector("#criar_lista_file");
+
+        _btnConfirmar.addEventListener("click", function () {
+            createdata(_criar_lista_name, _criar_lista_file);
+        });
+
+        _criar_lista.style.display = "";
+    }
+
+    async function createdata(name, input) {
+        let name_1 = name.normalize("NFD");
+        let name_2 = name_1.replace(/[\u0300-\u036f]/g, "");
+        let name_3 = name_2.toUpperCase();
+        let name_4 = name_3.replace(/ /g, "_");
+
+        const tableName = name;
+        const tableRef = name_4;
+
+        const myfile = input.files[0];
+
+        if (myfile) {
+            await sasjs.uploadFile('services/common/createdata', [{ "file": myfile, "fileName": myfile.name }], { "tableName": tableName, "tableRef": tableRef }).then((res) => {
+                console.log(res);
+            });
+        }
+    }
+    /* CREATEDATA END */
 });
+
+
+const foo = {
+    "START_DTTM": "15FEB22:12:53:46.342"
+    , "listas":
+        [
+            { "LIST_NAME": "CEIS", "TABLE_REFERENCE": "LISTA_CEIS" }
+            , { "LIST_NAME": "CNAE", "TABLE_REFERENCE": "LISTA_CNAE" }
+            , { "LIST_NAME": "GAFI (HIGH RISK)", "TABLE_REFERENCE": "LISTA_GAFI_HIGH_RISK" }
+            , { "LIST_NAME": "GAFI (IN PROGRESS)", "TABLE_REFERENCE": "LISTA_GAFI_IN_PROGRESS" }
+            , { "LIST_NAME": "IBAMA", "TABLE_REFERENCE": "LISTA_IBAMA" }
+            , { "LIST_NAME": "MUNICIPIO (FRONTEIRA", "TABLE_REFERENCE": "LISTA_MUNICIPIO_DE_FRONTEIRA" }
+            , { "LIST_NAME": "MUNICIPIO (RISCO)", "TABLE_REFERENCE": "LISTA_MUNICIPIO_RISCO" }
+            , { "LIST_NAME": "OFAC", "TABLE_REFERENCE": "LISTA_OFAC" }
+            , { "LIST_NAME": "ONU (CONSOLIDADA)", "TABLE_REFERENCE": "LISTA_ONU_CONSOLIDADA" }
+            , { "LIST_NAME": "PAIS (RISCO)", "TABLE_REFERENCE": "LISTA_PAIS_RISCO" }
+            , { "LIST_NAME": "PAIS (RISCO EU)", "TABLE_REFERENCE": "LISTA_PAIS_RISCO_EU" }
+            , { "LIST_NAME": "PAIS (RISCO ONU)", "TABLE_REFERENCE": "LISTA_PAIS_RISCO_ONU" }
+            , { "LIST_NAME": "PARAISO FISCAL", "TABLE_REFERENCE": "LISTA_PARAISO_FISCAL" }
+            , { "LIST_NAME": "PEP (AML)", "TABLE_REFERENCE": "LISTA_PEP_AML" }
+            , { "LIST_NAME": "PEP (SERASA)", "TABLE_REFERENCE": "LISTA_PEP_SERASA" }
+            , { "LIST_NAME": "PEP (SISCOAF)", "TABLE_REFERENCE": "LISTA_PEP_SISCOAF" }
+            , { "LIST_NAME": "PESSOA/NOME/CPF", "TABLE_REFERENCE": "LISTA_PESSOA_NOME_CPF" }
+            , { "LIST_NAME": "PROFISSOES", "TABLE_REFERENCE": "LISTA_PROFISSOES" }
+            , { "LIST_NAME": "RESTRITIVO INTERNO", "TABLE_REFERENCE": "LISTA_RESTRITIVO_INTERNO_PLDFT" }
+            , { "LIST_NAME": "RISCO (OFAC)", "TABLE_REFERENCE": "LISTA_RISCO_OFAC" }
+            , { "LIST_NAME": "SANCAO (ARMAMENTOS)", "TABLE_REFERENCE": "LISTA_SANCAO_ARMAMENTOS" }
+            , { "LIST_NAME": "SANCAO (INTERNA)", "TABLE_REFERENCE": "LISTA_SANCAO_INTERNA" }
+            , { "LIST_NAME": "TRABALHO ESCRAVO", "TABLE_REFERENCE": "LISTA_TRABALHO_ESCRAVO" }
+            , { "LIST_NAME": "UK BANK", "TABLE_REFERENCE": "LISTA_UK_BANCO" }
+            , { "LIST_NAME": "UNIAO EUROPEIA", "TABLE_REFERENCE": "LISTA_UNIAO_EUROPEIA" }
+        ]
+    , "SYSUSERID": "visouz"
+    , "MF_GETUSER": "visouz"
+    , "SYS_JES_JOB_URI": "/jobExecution/jobs/f43475ed-c4d3-423b-bdac-15ebb35bd1e5"
+    , "SYSJOBID": "1922922"
+    , "_DEBUG": ""
+    , "_PROGRAM": "/Public/app/ICL/services/common/appinit"
+    , "SYSCC": "0"
+    , "SYSERRORTEXT": ""
+    , "SYSHOSTNAME": "bpn09au"
+    , "SYSSCPL": "Linux"
+    , "SYSSITE": "70288264"
+    , "SYSVLONG": "V.03.05M0P111119"
+    , "SYSWARNINGTEXT": ""
+    , "END_DTTM": "15FEB22:12:53:46.420"
+}
